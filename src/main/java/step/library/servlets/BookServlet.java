234dee90bfc3b653ed4a53595ce0c0a494a5b4de
Denys Samoilenko <samojlenko_d@itstep.academy>
2021-10-14 10:45:29 +0300

    2021-10-14--2

--- src/main/java/step/library/servlets/BookServlet.java
+++ src/main/java/step/library/servlets/BookServlet.java
@@ -1,6 +1,9 @@
 package step.library.servlets;
 
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,14 +12,35 @@ import javax.servlet.http.Part;
 import java.io.IOException;
 
 @WebServlet("/books")
-
+@MultipartConfig  // !!1 Без этого multipart/form-data не работает
 public class BookServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // author, title, cover(file)
         String author = req.getParameter( "author" ) ;
         String title  = req.getParameter( "title" ) ;
-        Part cover = req.getPart( "cover" ) ;
+        Part   cover  = req.getPart( "cover" ) ;
+        int    resultStatus ;
+        String resultMessage ;
+        // TODO data validation
+        if( author == null || author.length() < 2 ) {
+            resultStatus  = -1 ;
+            resultMessage = "Author empty or too short" ;
+        } else if( title == null || title.length() < 2 ) {
+            resultStatus = -2 ;
+            resultMessage = "Title empty or too short" ;
+        } else if( cover.getSize() == 0 ) {
+            resultStatus = -3 ;
+            resultMessage = "Cover file required" ;
+        } else {
+            resultStatus = 1 ;
+            resultMessage = author + " " + title + " " + cover.getSubmittedFileName() ;
+        }
 
+        JSONObject result = new JSONObject();
+        result.put( "status",  resultStatus  ) ;
+        result.put( "message", resultMessage ) ;
+
+        resp.getWriter().print( result.toString() ) ;
     }
 }
--- web/js/bookaddform.js
+++ web/js/bookaddform.js
@@ -2,13 +2,17 @@ document.addEventListener("submit",sendClick);
 
 function sendClick(e) {
     e.preventDefault();
-
     // console.log(e.target); return;  // e.target - form
+    const author = e.target.querySelector("input[name=author]");
+    const title  = e.target.querySelector("input[name=title]");
+    const cover  = e.target.querySelector("input[name=cover]");
+    // TODO data validation
+
 
     const formData = new FormData();
-    formData.append("author", "");
-    fetch("/books", {
-        method: "POST",
-        body: formData
-    }).then(r => r.text()).then(console.log)
+    formData.append("author", author.value);
+    formData.append("title",  title.value);
+    formData.append("cover",  cover.files[0]);
+    fetch("books", { method: "POST", body: formData })
+        .then(r => r.text()).then(console.log)
 }
